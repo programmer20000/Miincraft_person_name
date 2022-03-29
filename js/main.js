ae6cc1401355c2df07c7f74fe6fae2ac8cd22a1b
programmer20000 <84572292+programmer20000@users.noreply.github.com>
2022-03-29 13:56:58 +0300

    was add files pjoject

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let FirstName = document.getElementById('FirstName');
+let SecondName = document.getElementById('SecondName');
+let ButtonSubmit = document.getElementById('ButtonSubmit');
+
+var fs = require('fs');
+
+function WritingDateInFile(error){
+    fs.appendFile(`date.txt ${FirstName.value} '\n' ${SecondName.value}`)
+    if(error) throw error;
+}
+
+ButtonSubmit.addEventListener('click', e=> WritingDateInFile())
